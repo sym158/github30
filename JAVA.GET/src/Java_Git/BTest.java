b237dfc8f4bd66e43bcbc3b873da12bb7aa705c2
KO_09 <KO_09@KO_09-HP>
2018-01-02 12:35:50 +0900

    BTest 두번째 커밋~~~~~A 사용자

--- JAVA.GET/src/Java_Git/BTest.java
+++ JAVA.GET/src/Java_Git/BTest.java
@@ -3,6 +3,7 @@ package Java_Git;
 public class BTest {
 	public static void main(String[] args) {
 		System.out.println("BTest 첫번째 커밋~~~~~");
+		System.out.println("BTest 두번째 커밋~~~~~A 사용자");
 	}
 
 }
